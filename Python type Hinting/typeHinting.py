8faef6346e7ed75d2bdf12ee1f2d684a71921f3b
Hafeeda <kphafeeda.work@gmail.com>
2026-05-21 18:40:29 +0530

    Day1 and Day2 Task

---
+++ Python type Hinting/typeHinting.py
@@ -0,0 +1,15 @@
+
+# This variable stores developer name
+
+developer_name: str = "Hafeeda"
+
+print(developer_name)
+# This variable  stores integer variable
+
+learning_days: int = 120
+
+print(learning_days)
+# this is also string variable
+my_goal: str = "Ai Engineer "
+
+print(my_goal)
